79c3d31c696c3988142aa8269ce34e17251423be
chanhtv235 <chanhtv235@gmail.com>
2025-09-24 20:28:42 +0700

    update

--- demo_chatbox_gemini/src/main/java/org/example/demo_chatbox_gemini/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package org.example.demo_chatbox_gemini;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
